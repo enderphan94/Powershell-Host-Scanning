cc05cc4bec89bf56578a32ca6579edf1daf3ab66
Ender Loc Phan <weintome@gmail.com>
2017-04-12 23:42:53 +0300

    Add files via upload

---
+++ discover.ps1
@@ -0,0 +1,134 @@
+
+param( 
+        #[parameter(Mandatory=$true)] 
+            
+        [System.Net.IPAddress]$StartScanIP, 
+        [System.Net.IPAddress]$EndScanIP, 
+        [Int[]]$Ports
+   
+       
+    ) 
+
+    Begin{} 
+
+    Process 
+    {
+   if($StartScanIP -ne $null){
+        $ScanIPRange = @() 
+            if($EndScanIP -ne $null) 
+            { 
+
+                $StartIP = $StartScanIP -split '\.' 
+              
+                  [Array]::Reverse($StartIP)   
+                  $StartIP = ([System.Net.IPAddress]($StartIP -join '.')).Address  
+              
+                $EndIP = $EndScanIP -split '\.' 
+                  [Array]::Reverse($EndIP)   
+                  $EndIP = ([System.Net.IPAddress]($EndIP -join '.')).Address  
+         
+                For ($x=$StartIP; $x -le $EndIP; $x++) {     
+                    $IP = [System.Net.IPAddress]$x -split '\.'          
+                    [Array]::Reverse($IP)    
+                    $ScanIPRange += $IP -join '.'  
+                } 
+                 
+            } 
+            else 
+            { 
+                $ScanIPRange = $StartScanIP 
+                Write-Host $ScanIPRange
+            }  
+     
+            Write-Verbose "Start scaning..." 
+          
+            Foreach($IP in $ScanIPRange) 
+            {
+           
+                #HOSTNAME
+                if(Test-Connection -ComputerName $IP -Quiet) 
+                    {
+                        Try 
+                            { 
+                                $HostName = [System.Net.Dns]::GetHostbyAddress($IP).HostName 
+                            } 
+                        Catch 
+                            { 
+                                $HostName = $null 
+                            } 
+                  
+                        
+                        #MAC
+                         Try 
+                            { 
+                                $result= nbtstat -A $IP | select-string "MAC" 
+                                $MAC = [string]([Regex]::Matches($result, "([0-9A-F][0-9A-F]-){5}([0-9A-F][0-9A-F])")) 
+                            } 
+                         Catch 
+                            { 
+                                $MAC = $null 
+                  
+                            }
+                        #port
+                        $PortsStatus = @() 
+                        ForEach($Port in $Ports) 
+                        { 
+                            Try 
+                            {                             
+                                $TCPClient = new-object Net.Sockets.TcpClient 
+                                $TCPClient.Connect($IP, $Port) 
+                                $TCPClient.Close() 
+
+                              $PortStatus = New-Object PSObject -Property @{             
+                                    Port        = $Port 
+                                } 
+                                $PortsStatus += $PortStatus
+                               
+                                
+                            }     
+                            Catch 
+                            { 
+                                 
+                                $PortsStatus=$null 
+                         }   
+                        }
+                        #OS
+                        if($HostName) 
+                        { 
+                            $Result = Invoke-Command -ComputerName $HostName -ScriptBlock {systeminfo} -ErrorAction SilentlyContinue  
+                        } 
+                        else 
+                        { 
+                            $Result = Invoke-Command -ComputerName $IP -ScriptBlock {systeminfo} -ErrorAction SilentlyContinue  
+                        } 
+                         
+                        if($Result -ne $null) 
+                        { 
+                            if($OS_Name -eq $null) 
+                            { 
+                                $OS_Name = ($Result[2..3] -split ":\s+")[1] 
+                                $OS_Ver = ($Result[2..3] -split ":\s+")[3] 
+                            }     
+                            $WinRMAccess = $true 
+                        } 
+                        else 
+                        { 
+                            $WinRMAccess = $false 
+                        } 
+                        
+                    } 
+                    
+                    
+            
+            Write-Host $IP  $HostName  $MAC $PortsStatus $OS_Name $OS_Ver
+
+          
+            
+            }
+        
+        
+        }
+
+
+    }
+   
---
+++ request.ps1
@@ -0,0 +1,69 @@
+Param(
+  [string]$url,
+  [string]$ip
+)
+Begin{}
+Process 
+    {
+$ErrorActionPreference = "SilentlyContinue"
+
+if($url){
+
+$Target= @()
+
+foreach($line in Get-Content .\list.txt) {
+   $new_url = ($url + $line)
+
+   $HTTP_Request = [System.Net.WebRequest]::Create($new_url)
+
+   $HTTP_Response = $HTTP_Request.GetResponse()
+
+
+   $HTTP_Status = [int]$HTTP_Response.StatusCode
+
+   If ($HTTP_Status -eq 200) { 
+    $Target += $new_url
+
+   }
+
+}
+Write-Host "Listing below are paths of the website:"
+Write-Host $Target
+}
+
+
+if($ip){
+     $result = $null
+
+    
+
+     $currentEAP = $ErrorActionPreference
+
+     $ErrorActionPreference = "silentlycontinue"
+
+
+     $result = [System.Net.Dns]::gethostentry($ip)
+
+
+     $ErrorActionPreference = $currentEAP
+
+    
+
+     If ($Result)
+
+     {
+
+          Write-Host $Result.HostName
+
+     }
+
+     Else
+
+     {
+
+          Write-Host "$IP – No HostNameFound"
+
+     }
+ }
+
+}
